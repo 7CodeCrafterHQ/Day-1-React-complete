76b747184443f8dbf2d553be2bbe4fb20e705074
7CodeCrafterHQ <7codecrafterhq@gmail.com>
2024-03-14 17:26:43 +0530

    dada base connection and make router , install morgan

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 require("dotenv").config({path:"./.env"})
 const express = require("express");
 const app = express();
+// database connection
+require("./models/dbconnections").dbconnection(); 
+const userRouter = require("./router/userRouter")
 
+//logger
+const logger = require("morgan")
+app.use(logger("tiny"));
+// body pasrser
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+//routes
+app.use("/api/user",userRouter)
+// http://localhost:8080/api/user
+// http://localhost:8080/api/user/create
+
+
+// server
 app.listen(process.env.PORT ,()=>{
     console.log(`The Server is running on port ${process.env.PORT}`);
 });
